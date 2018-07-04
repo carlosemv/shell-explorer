6b5de15481b83afec4c4c77b326457395a25e9f4
Carlos Vieira <vieiramecarlos@gmail.com>
2018-07-04 01:46:01 -0300

    change path handling in shell

--- shell.py
+++ shell.py
@@ -23,59 +23,60 @@ class Shell(EventDispatcher):
 	def command(self, instance):
 		lexer = ShellLexer()
 		lexed = [{'token':t, 'value':v} for t,v in lexer.get_tokens(instance.text)]
-		sublists = [seq for seq in split_list(lexed, Token.Text.Whitespace, lambda l : l['token']) if seq]
-		if sublists[0][0]['token'] == Token.Text and sublists[0][0]['value'] == "sudo":
-			print("Erro: Tentativa de comando sudo")
-			return
-		args = [tok['value'] for sublist in sublists for tok in sublist]
-		if sublists[0][0]['token'] == Name.Builtin:
-			cmd, n_args = args[0], len(args)
-			if cmd == 'touch' and n_args == 2:
-				self.touch(args[1])
-			elif cmd == 'rm' and n_args == 2:
-				self.rm(args[1])
-			elif (cmd == 'rm' and n_args == 3 and args[1] == '-d') or \
-					(cmd == 'rmdir' and n_args == 2):
-				self.rmdir(args[-1])
-			elif cmd == 'rm' and n_args == 3 and args[1] == '-r':
-				self.rmtree(args[2])
-			elif cmd == 'mkdir' and n_args == 2:
-				self.mkdir(args[1])
-			elif cmd == 'cp' and n_args == 3:
-				self.cp(args[1], args[2])
-			elif cmd == 'cp' and n_args == 4 and args[1] == '-r':
-				self.cptree(args[2], args[3])
-			elif cmd == 'mv' and n_args == 3:
-				self.mv(args[1], args[2])
-			elif cmd == 'cd' and n_args > 1:
-				prev_pont = True
-				arg = ""
-				bad = False
-				for tok in [tmp for sublist in sublists for tmp in sublist][1:]:
-					if tok['token'] == Token.Punctuation:
-						arg = arg + tok['value']
-						prev_pont = True
-					elif prev_pont:
-						arg = arg + tok['value']
-						prev_pont = False
-					else:
-						bad = True
-						break
-				if not bad:
-					prev_path = self.path
-					try:
-						self.path = normpath(join(self.path, arg))
-					except FileNotFoundError:
-						self.path = prev_path
-					except NotADirectoryError:
-						self.path = prev_path
-				else:
-					self.run(instance.text.split())
-			else:
-				self.run(instance.text.split())
+		lex_seqs = [seq for seq in split_list(lexed, Token.Text.Whitespace, lambda l : l['token']) if seq]
+
+		args = []
+		for seq in lex_seqs:
+			value = ''.join(lex['value'] for lex in seq)
+			token = seq[0]['token']
+			if token == Token.Punctuation:
+				token = Token.Text
+			args.append({'token':token, 'value':value})
+		print(args)
+
+		if args[0]['token'] == Name.Builtin:
+			self.builtin(args)
 		else:
 			self.run(instance.text.split())
-	
+
+		self.dispatch('on_update')
+
+	def builtin(self, args):
+		cmd, n_args = args[0]['value'], len(args)
+
+		is_path = [False for i in range(max(n_args, 4))]
+		for i, arg in enumerate(args):
+			if arg['token'] == Token.Text:
+				arg['value'] = normpath(join(self.path, arg['value']))
+				is_path[i] = True
+
+		if cmd == 'touch' and n_args == 2 and is_path[1]:
+			self.touch(args[1]['value'])
+		elif cmd == 'rm':
+			if n_args == 3 and args[1]['token'] == Name.Attribute and is_path[2]:
+				if args[1]['value'] == '-d':
+					self.rmdir(args[2]['value'])
+				elif args[1]['value'] == '-r':
+					self.rmtree(args[2]['value'])
+			elif n_args == 2 and is_path[1]:
+				self.rm(args[1]['value'])
+		elif cmd == 'rmdir' and n_args == 2 and is_path[1]:
+			self.rmdir(args[1]['value'])
+		elif cmd == 'mkdir' and n_args == 2 and is_path[1]:
+			self.mkdir(args[1]['value'])
+		elif cmd == 'cp':
+			if n_args == 4 and args[1]['token'] == Name.Attribute and \
+					args[1]['value'] == '-r' and is_path[2] and is_path[3]:
+				self.cptree(args[2]['value'], args[3]['value'])
+			elif n_args == 3 and is_path[1] and is_path[2]:
+				self.cp(args[1]['value'], args[2]['value'])
+		elif cmd == 'mv' and n_args == 3 and is_path[1] and is_path[2]:
+			self.mv(args[1]['value'], args[2]['value'])
+		elif cmd == 'cd' and n_args == 2 and is_path[1]:
+			self.cd(args[1]['value'])
+		else:
+			self.run(instance.text.split())
+
 	def list_dir(path):
 		contents = os.listdir(path)
 		return {file : not isfile(join(path, file)) for file in contents}
@@ -95,61 +96,69 @@ class Shell(EventDispatcher):
 	def on_update(shell):
 		pass
 
-	def touch(self, name):
-		f = open(join(self.path, name),"w+")
+	def touch(self, path):
+		f = open(path,"w+")
 		f.close()
 
-	def rm(self, name):
+	def rm(self, path):
 		try:
-			os.remove(join(self.path,name))
+			os.remove(path)
 		except FileNotFoundError:
 			pass
 		except IsADirectoryError:
 			pass
 
-	def rmdir(self, name):
+	def rmdir(self, path):
 		try:
-			os.rmdir(join(self.path,name))
+			os.rmdir(path)
 		except FileNotFoundError:
 			pass
 		except OSError:
 			pass
 
-	def rmtree(self, name):
+	def rmtree(self, path):
 		try:
-			shutil.rmtree(join(self.path,name))
+			shutil.rmtree(path)
 		except FileNotFoundError:
 			pass
 		except NotADirectoryError:
 			pass
 	
-	def mkdir(self, name):
+	def mkdir(self, path):
 		try:
-			os.mkdir(join(self.path,name))
+			os.mkdir(path)
 		except FileExistsError:
 			pass
 			
-	def cp(self, a, b):
+	def cp(self, path_src, path_tgt):
 		try:
-			shutil.copy2(join(self.path,a), join(self.path,b))
+			shutil.copy2(path_src, path_tgt)
 		except FileNotFoundError:
 			pass
 		except IsADirectoryError:
 			pass
 	
-	def cptree(self, a, b):
+	def cptree(path_src, path_tgt):
 		try:
-			shutil.copytree(join(self.path,a), join(self.path,b))
+			shutil.copytree(path_src, path_tgt)
 		except FileNotFoundError:
 			pass
 		except NotADirectoryError:
 			pass
 	
-	def mv(self, a, b):
+	def mv(self, path_src, path_tgt):
 		try:
-			shutil.move(join(self.path,a), join(self.path,b))
+			shutil.move(path_src, path_tgt)
 		except FileNotFoundError:
 			pass
 		except FileExistsError:
 			pass
 
+	def cd(self, new_path):
+		prev_path = self.path
+		try:
+			self.path = new_path
+		except FileNotFoundError:
+			self.path = prev_path
+		except NotADirectoryError:
+			self.path = prev_path
